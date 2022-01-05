c69fddca2427d2aa153ff21a4d2b74d01f6b3cf7
climbingdaily <yudidai@stu.xmu.edu.cn>
2022-01-05 16:14:25 +0800

    添加semanticposs读取

--- core/datasets/semantic_kitti.py
+++ core/datasets/semantic_kitti.py
@@ -78,14 +78,14 @@ class SemanticKITTI(dict):
         else:
             super().__init__({
                 'train':
-                    SemanticKITTIInternal(root,
+                    SemanticPOSSInternal(root,
                                           voxel_size,
                                           num_points,
                                           sample_stride=1,
                                           split='train',
                                           google_mode=google_mode),
                 'test':
-                    SemanticKITTIInternal(root,
+                    SemanticPOSSInternal(root,
                                           voxel_size,
                                           num_points,
                                           sample_stride=sample_stride,
@@ -174,6 +174,148 @@ class SemanticKITTIInternal:
     def __getitem__(self, index):
         with open(self.files[index], 'rb') as b:
             block_ = np.fromfile(b, dtype=np.float32).reshape(-1, 4)
+        block = np.zeros_like(block_)
+
+        if 'train' in self.split:
+            theta = np.random.uniform(0, 2 * np.pi)
+            scale_factor = np.random.uniform(0.95, 1.05)
+            rot_mat = np.array([[np.cos(theta), np.sin(theta), 0],
+                                [-np.sin(theta),
+                                 np.cos(theta), 0], [0, 0, 1]])
+
+            block[:, :3] = np.dot(block_[:, :3], rot_mat) * scale_factor
+        else:
+            theta = self.angle
+            transform_mat = np.array([[np.cos(theta),
+                                       np.sin(theta), 0],
+                                      [-np.sin(theta),
+                                       np.cos(theta), 0], [0, 0, 1]])
+            block[...] = block_[...]
+            block[:, :3] = np.dot(block[:, :3], transform_mat)
+
+        block[:, 3] = block_[:, 3]
+        pc_ = np.round(block[:, :3] / self.voxel_size).astype(np.int32)
+        pc_ -= pc_.min(0, keepdims=1)
+
+        label_file = self.files[index].replace('velodyne', 'labels').replace(
+            '.bin', '.label')
+        if os.path.exists(label_file):
+            with open(label_file, 'rb') as a:
+                all_labels = np.fromfile(a, dtype=np.int32).reshape(-1)
+        else:
+            all_labels = np.zeros(pc_.shape[0]).astype(np.int32)
+
+        labels_ = self.label_map[all_labels & 0xFFFF].astype(np.int64)
+
+        feat_ = block
+
+        _, inds, inverse_map = sparse_quantize(pc_,
+                                               return_index=True,
+                                               return_inverse=True)
+
+        if 'train' in self.split:
+            if len(inds) > self.num_points:
+                inds = np.random.choice(inds, self.num_points, replace=False)
+
+        pc = pc_[inds]
+        feat = feat_[inds]
+        labels = labels_[inds]
+        lidar = SparseTensor(feat, pc)
+        labels = SparseTensor(labels, pc)
+        labels_ = SparseTensor(labels_, pc_)
+        inverse_map = SparseTensor(inverse_map, pc_)
+
+        return {
+            'lidar': lidar,
+            'targets': labels,
+            'targets_mapped': labels_,
+            'inverse_map': inverse_map,
+            'file_name': self.files[index]
+        }
+
+    @staticmethod
+    def collate_fn(inputs):
+        return sparse_collate_fn(inputs)
+
+class SemanticPOSSInternal:
+
+    def __init__(self,
+                 root,
+                 voxel_size,
+                 num_points,
+                 split,
+                 sample_stride=1,
+                 submit=False,
+                 google_mode=True):
+        if submit:
+            trainval = True
+        else:
+            trainval = False
+        self.root = root
+        self.split = split
+        self.voxel_size = voxel_size
+        self.num_points = num_points
+        self.sample_stride = sample_stride
+        self.google_mode = google_mode
+        self.seqs = []
+        if split == 'train':
+            self.seqs = [
+                '00', '01', '02', '03', '04'
+            ]
+            if self.google_mode or trainval:
+                self.seqs.append('05')
+        elif self.split == 'val':
+            self.seqs = ['05']
+        elif self.split == 'test':
+            self.seqs = ['05']
+
+        self.files = []
+        for seq in self.seqs:
+            seq_files = sorted(
+                os.listdir(os.path.join(self.root, seq, 'velodyne')))
+            seq_files = [
+                os.path.join(self.root, seq, 'velodyne', x) for x in seq_files
+            ]
+            self.files.extend(seq_files)
+
+        if self.sample_stride > 1:
+            self.files = self.files[::self.sample_stride]
+
+        reverse_label_name_mapping = {}
+        self.label_map = np.zeros(260)
+        cnt = 0
+        for label_id in label_name_mapping:
+            if label_id > 250:
+                if label_name_mapping[label_id].replace('moving-',
+                                                        '') in kept_labels:
+                    self.label_map[label_id] = reverse_label_name_mapping[
+                        label_name_mapping[label_id].replace('moving-', '')]
+                else:
+                    self.label_map[label_id] = 255
+            elif label_id == 0:
+                self.label_map[label_id] = 255
+            else:
+                if label_name_mapping[label_id] in kept_labels:
+                    self.label_map[label_id] = cnt
+                    reverse_label_name_mapping[
+                        label_name_mapping[label_id]] = cnt
+                    cnt += 1
+                else:
+                    self.label_map[label_id] = 255
+
+        self.reverse_label_name_mapping = reverse_label_name_mapping
+        self.num_classes = cnt
+        self.angle = 0.0
+
+    def set_angle(self, angle):
+        self.angle = angle
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        with open(self.files[index], 'rb') as b:
+            block_ = np.fromfile(b, dtype=np.float32).reshape(-1, 4)    #x,y,z,intensity
         block = np.zeros_like(block_)
 
         if 'train' in self.split:
